ded7f1d75f909d72fac36c583921b06d4d30bf8f
katyusya <katusha.fedyakena@mail.ru>
2023-07-09 22:42:53 +0300

    init project

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import './banner.scss';
+
+export default function Banner () {
+    return(
+        <section className="banner">
+    {/* <Image
+      src="banner.jpg"
+      width={500}
+      height={500}
+      alt="Banner"
+    /> */}
+        </section>
+    )
+}
