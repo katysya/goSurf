bdd85912a2a92827b702e0b55315d817d741c9a8
katyusya <katusha.fedyakena@mail.ru>
2023-07-27 19:58:32 +0300

    Стилизация SwiperBeach

--- src/components/Swipers/BeachSwiper/BeachSwiper.tsx
+++ src/components/Swipers/BeachSwiper/BeachSwiper.tsx
@@ -1,35 +1,223 @@
 'use client';
-import { Navigation, Pagination } from 'swiper/modules';
+import {
+  Autoplay,
+  EffectFade,
+  Navigation,
+  Pagination,
+} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useRef } from 'react';
+import { Swiper as SwiperCore } from 'swiper/types';
+import Image from 'next/image';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './beachSwiper.scss';
 
+import arrowRight from '../../../assets/images/mainSwiper/swiperRight.svg';
+import arrowLeft from '../../../assets/images/mainSwiper/swiperLeft.svg';
+import Slide1 from '../../../assets/images/beachSwiper/img1.jpg';
+import Slide2 from '../../../assets/images/beachSwiper/img2.jpg';
+import Slide3 from '../../../assets/images/beachSwiper/img3.jpg';
+import Slide4 from '../../../assets/images/beachSwiper/img4.jpg';
+
 export default function BeachSwiper() {
+  const swiperRef = useRef<SwiperCore>();
+
   return (
     <div className="beachSwiper">
+      <div className="beachSwiper__top">
+        <div className="beachSwiper__navigation">
+          <button
+            className="beachSwiper__navigation-btn"
+            onClick={() => swiperRef.current?.slidePrev()}
+          >
+            <Image src={arrowLeft} width={25} height={20} alt="" />
+          </button>
+          <button
+            className="beachSwiper__navigation-btn"
+            onClick={() => swiperRef.current?.slideNext()}
+          >
+            <Image src={arrowRight} width={25} height={20} alt="" />
+          </button>
+        </div>
+      </div>
       <div className="beachSwiper__swiper">
         <Swiper
           pagination={{
             clickable: true,
           }}
-          loop
-          modules={[Navigation, Pagination]}
-          spaceBetween={10}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+          }}
+          modules={[Autoplay, EffectFade, Navigation, Pagination]}
           slidesPerView={4}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Malibu Beach
+              </div>
+              <Image
+                src={Slide1}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                California | USA
+              </div>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Airlie Beach
+              </div>
+              <Image
+                src={Slide2}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                Queensland | Australia
+              </div>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Cloud Nine
+              </div>
+              <Image
+                src={Slide3}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                Siargao | Philippines
+              </div>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Vieux Boucau
+              </div>
+              <Image
+                src={Slide4}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                Hossegor | France
+              </div>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Malibu Beach
+              </div>
+              <Image
+                src={Slide1}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                California | USA
+              </div>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Airlie Beach
+              </div>
+              <Image
+                src={Slide2}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                Siargao | Philippines
+              </div>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Cloud Nine
+              </div>
+              <Image
+                src={Slide3}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                Siargao | Philippines
+              </div>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="#"
+              target="_blank"
+              className="beachSwiper__swiper-slide"
+            >
+              <div className="beachSwiper__swiper-name">
+                Vieux Boucau
+              </div>
+              <Image
+                src={Slide4}
+                layout="fill"
+                objectFit="cover"
+                alt=""
+              />
+              <div className="beachSwiper__swiper-place">
+                Siargao | Philippines
+              </div>
+            </a>
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
