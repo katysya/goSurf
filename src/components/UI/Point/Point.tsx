1bc3a189b8dcb63c486009f70ed4c48092a1fd8d
katyusya <katusha.fedyakena@mail.ru>
2023-09-05 12:29:50 +0300

    Добавление Point на Map

---
+++ src/components/UI/Point/Point.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image';
+
+import './Point.scss';
+
+import Surf from '../../../../public/Map/Surf.svg';
+import Water from '../../../../public/Map/Water.svg';
+import Winde from '../../../../public/Map/Wind.svg';
+import Arrow from '../../../../public/Map/Arrow.svg';
+
+interface IPoint {
+  id: Number;
+  position: Object;
+  beach: String;
+  place: String;
+  surf: String;
+  tide: String;
+  wind: String;
+}
+
+export default function Point({
+  id,
+  position,
+  beach,
+  place,
+  surf,
+  tide,
+  wind,
+}: IPoint) {
+  return (
+    <div className="point" style={position}>
+      <div className="point__info">
+        <div className="point__beach">
+          <div className="point__top">
+            <div className="point__arrow">
+              <div className="point__name">{beach}</div>
+              <Image src={Arrow} alt="" />
+            </div>
+            <div className="point__place">{place}</div>
+          </div>
+          <div className="point__data">
+            <div className="point__data-block">
+              <Image src={Surf} alt="" />
+              <p className="point__data-block-name">{surf}</p>
+              <p className="point__data-block-desc">Surf (FT)</p>
+            </div>
+            <div className="point__data-block">
+              <Image src={Water} alt="" />
+              <p className="point__data-block-name">{tide}</p>
+              <p className="point__data-block-desc">Surf (FT)</p>
+            </div>
+            <div className="point__data-block">
+              <Image src={Winde} alt="" />
+              <p className="point__data-block-name">{wind}</p>
+              <p className="point__data-block-desc">Surf (FT)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="point__circle"></div>
+    </div>
+  );
+}
--- src/components/section/Map/Map.tsx
+++ src/components/section/Map/Map.tsx
@@ -4,9 +4,119 @@ import Image from 'next/image';
 import './map.scss';
 
 import map from '../../../../public/Map/map.png';
-import points from '../../../../public/Map/Map.svg';
+import Point from '@/components/UI/Point/Point';
 
 export default function Map() {
+  const points = [
+    {
+      id: 1,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '4 SE',
+      position: {
+        left: '0%',
+        top: '49%',
+      },
+    },
+    {
+      id: 2,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '13%',
+        top: '46%',
+      },
+    },
+    {
+      id: 3,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '28%',
+        top: '50%',
+      },
+    },
+    {
+      id: 4,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '35%',
+        top: '76%',
+      },
+    },
+    {
+      id: 5,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '45%',
+        top: '35%',
+      },
+    },
+    {
+      id: 6,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '53%',
+        top: '87%',
+      },
+    },
+    {
+      id: 7,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '66%',
+        top: '61%',
+      },
+    },
+    {
+      id: 8,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '91%',
+        top: '80%',
+      },
+    },
+    {
+      id: 9,
+      beach: 'Airlie Beach',
+      place: 'Australia',
+      surf: '9 - 13',
+      tide: '+2.3',
+      wind: '+2.3',
+      position: {
+        left: '88%',
+        top: '36%',
+      },
+    },
+  ];
   return (
     <section className="map" id="map">
       <Title title="Surf" />
@@ -19,6 +129,20 @@ export default function Map() {
         />
       </div>
       <div className="map__image">
+        {points.map((item) => {
+          return (
+            <Point
+              id={item.id}
+              position={item.position}
+              beach={item.beach}
+              place={item.place}
+              surf={item.surf}
+              tide={item.tide}
+              wind={item.wind}
+              key={item.id}
+            />
+          );
+        })}
         <Image
           src={map}
           fill
