fedd5a3bcdcfb09016e7efaf80f1fae939bb9368
katyusya <katusha.fedyakena@mail.ru>
2023-10-05 18:31:47 +0300

    Добавление анимации самолёта

--- src/components/Swipers/InfoSwiper/InfoSwiper.tsx
+++ src/components/Swipers/InfoSwiper/InfoSwiper.tsx
@@ -147,18 +147,9 @@ export default function InfoSwiper({ info }: IInfo) {
           })}
           <motion.div
             className="infoSwiper__airline"
-            animate={{
-              scale: [1, 2, 2, 1, 1],
-              rotate: [0, 0, 180, 180, 0],
-              borderRadius: ['0%', '0%', '50%', '50%', '0%'],
-            }}
-            transition={{
-              duration: 2,
-              ease: 'easeInOut',
-              times: [0, 0.2, 0.5, 0.8, 1],
-              repeat: Infinity,
-              repeatDelay: 1,
-            }}
+            initial={{ scale: 1, right: '-20%', top: '40%' }}
+            whileInView={{ scale: 2.5, right: '120%', top: '10%' }}
+            transition={{ duration: 6 }}
           >
             <div className="infoSwiper__slide-block-img">
               <Image src={Airline} fill alt="" />
